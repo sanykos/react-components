f492a1cdf8d8f8203edab95523390035143e830d
Alexander <alexander.kosykh.fc@gmail.com>
2021-01-24 21:00:45 +0300

    Update SandBox with prettier

--- src/Layout/Components/Button/sandbox.js
+++ src/Layout/Components/Button/sandbox.js
@@ -1,30 +1,35 @@
-import React, {Fragment} from 'react'
-import Button from './Button'
-
+import React, { Fragment } from 'react';
+import Button from './Button';
 
 const SandBox = () => (
+  <Fragment>
+    <h2>Btn on click</h2>
+    <hr />
+    <Button onClick={() => console.log('!!!')}></Button>
+    <h2>Btn disabled</h2>
+    <hr />
+    <Button onClick={() => console.log('!!!')} disabled>
+      Btn text
+    </Button>
+    <h2>Btn dataname</h2>
+    <hr />
+    <Button data-name="Button" onClick={() => console.log('!!!')}>
+      Btn text
+    </Button>
+    <h2>Btn type</h2>
+    <hr />
+    <Button type="submit" onClick={() => console.log('!!!')}>
+      Btn text
+    </Button>
+    <h2>Btn link</h2>
+    <hr />
+    <Button href="text" onClick={() => console.log('!!!')}>
+      Btn text
+    </Button>
+    <Button href="text" onClick={() => console.log('!!!')} disabled>
+      Btn text
+    </Button>
+  </Fragment>
+);
 
-    <Fragment>
-        <h2>Btn on click</h2>
-        <hr/>
-        <Button onClick={()=> console.log('!!!')}></Button>
-        <h2>Btn disabled</h2>
-        <hr/>
-        <Button onClick={()=> console.log('!!!')} disabled>Btn text</Button>
-        <h2>Btn dataname</h2>
-        <hr/>
-        <Button data-name="Button" onClick={()=> console.log('!!!')}>Btn text</Button>
-        <h2>Btn type</h2>
-        <hr/>
-        <Button type="submit" onClick={()=> console.log('!!!')}>Btn text</Button>
-        <h2>Btn link</h2>
-        <hr/>
-        <Button href="text" onClick={()=> console.log('!!!')}>Btn text</Button>
-        <Button href="text" onClick={()=> console.log('!!!')} disabled>Btn text</Button>
-    </Fragment>
-
-
-)
-
-
-export default SandBox
+export default SandBox;
